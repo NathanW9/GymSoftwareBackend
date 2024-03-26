eb3b90816c16645257fffa160e0a5a9d917b88f8
KRB001 <kieranbentley0@gmail.com>
2024-03-26 10:32:44 -0400

    initial spring utilities

---
+++ src/main/java/com/team4/gymsoftware/GymsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.team4.gymsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymsoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team4/gymsoftware/GymsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.gymsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
