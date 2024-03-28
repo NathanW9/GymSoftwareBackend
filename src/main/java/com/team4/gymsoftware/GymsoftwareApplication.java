b9d61ab8549039050b06eda228d7d80a5c8cc325
KRB001 <kieranbentley0@gmail.com>
2024-03-28 11:39:01 -0400

    config and boilerplate prep for db

--- src/main/java/com/team4/gymsoftware/GymsoftwareApplication.java
+++ src/main/java/com/team4/gymsoftware/GymsoftwareApplication.java
@@ -2,9 +2,12 @@ package com.team4.gymsoftware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class GymsoftwareApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymsoftwareApplication.class, args);
