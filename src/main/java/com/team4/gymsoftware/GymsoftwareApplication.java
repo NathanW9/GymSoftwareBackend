88194488715cbd710dfd24faa386db5294b66c5e
KRB001 <kieranbentley0@gmail.com>
2024-03-29 10:45:33 -0400

    remove debug code

--- src/main/java/com/team4/gymsoftware/GymsoftwareApplication.java
+++ src/main/java/com/team4/gymsoftware/GymsoftwareApplication.java
@@ -19,33 +19,4 @@ public class GymsoftwareApplication {
 
 	}
 
-	@Bean
-	CommandLineRunner runner(GymUserRepository gymUserRepository,
-							 TrainerRepository trainerRepository,
-							 RequestRepository requestRepository) {
-		return args -> {
-
-			GymUser gymUser = new GymUser();
-			gymUser.setName("John");
-
-			gymUserRepository.save(gymUser);
-			GymUser savedUser = gymUserRepository.findById(gymUser.getId()).orElseThrow(NoSuchElementException::new);
-
-			Trainer trainer = new Trainer();
-			trainer.setName("Joe");
-
-			trainerRepository.save(trainer);
-			Trainer savedTrainer = trainerRepository.findById(trainer.getId()).orElseThrow(NoSuchElementException::new);
-
-			savedUser.setTrainer(savedTrainer);
-			gymUserRepository.save(savedUser);
-
-			Request request = new Request();
-			request.setRequester(savedUser);
-			request.setReceiver(savedTrainer);
-
-			requestRepository.save(request);
-		};
-	}
-
 }
