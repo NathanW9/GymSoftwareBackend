510f7e11f05386a35d26b7f537a50c36928e9743
KRB001 <kieranbentley0@gmail.com>
2024-04-15 15:53:34 -0400

    create trainer request

---
+++ src/main/java/com/team4/gymsoftware/dto/CreateTrainerRequest.java
@@ -0,0 +1,4 @@
+package com.team4.gymsoftware.dto;
+
+public record CreateTrainerRequest(String name) {
+}
---
+++ src/main/java/com/team4/gymsoftware/services/TrainerService.java
@@ -0,0 +1,4 @@
+package com.team4.gymsoftware.services;
+
+public interface TrainerService {
+}
